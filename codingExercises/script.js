6464ae1475b21f22065adf739ced91996c760b52
KOTFAN <kotfan.empire@gmail.com>
2025-03-03 17:16:23 +0200

    add 2

--- codingExercises/script.js
+++ codingExercises/script.js
@@ -1,16 +1,27 @@
 //Challenge #1
 
-let massMark = 78
-let heightMark = 1.69
-let massJohn = 92
-let heightJohn = 1.76
+// let massMark = 78
+// let heightMark = 1.69
+// let massJohn = 92
+// let heightJohn = 1.76
 
-let BMIMark = massMark / (heightMark * heightMark)
-let BMIJohn = massJohn / (heightJohn * heightJohn)
+// let BMIMark = massMark / (heightMark * heightMark)
+// let BMIJohn = massJohn / (heightJohn * heightJohn)
 
-console.log(BMIMark, BMIJohn)
-let markHigherBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn)
+// let markHigherBMI = BMIMark > BMIJohn;
 
-console.log(markHigherBMI)
+// console.log(markHigherBMI)
 
+////Challenge #2
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+console.log(BMIMark > BMIJohn ? `Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!` : `John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
 
