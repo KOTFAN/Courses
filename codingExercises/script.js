6ced0c534cbe1dc9a9e8bd7aaffe97b9504603b1
KOTFAN <kotfan.empire@gmail.com>
2025-03-03 18:12:57 +0200

    add 7

--- codingExercises/script.js
+++ codingExercises/script.js
@@ -62,13 +62,38 @@
 
 //Challenge #6
 
-let bills = [125, 555, 44];
+// let bills = [125, 555, 44];
 
-const calcTip = (bill) => {
-   return 50 <= bill && bill <= 300 ? bill * 0.15 : bill * 0.2
+// const calcTip = (bill) => {
+//    return 50 <= bill && bill <= 300 ? bill * 0.15 : bill * 0.2
+// }
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+// console.log(tips)
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+// console.log(totals)
+// console.log(bills)
+
+
+
+//Challenge #7
+const mark = {
+   fullName: "Mark Miller",
+   mass: 78,
+   height: 1.69,
+   calcBMI: function () {
+      this.bmi = this.mass / (this.height * this.height)
+      return this.bmi
+   }
 }
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
-console.log(tips)
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
-console.log(totals)
-console.log(bills)
+
+const john = {
+   fullName: "John Smith",
+   mass: 92,
+   height: 1.95,
+   calcBMI: function () {
+      this.bmi = this.mass / (this.height * this.height)
+      return this.bmi
+   }
+}
+
+console.log(john.calcBMI() > mark.calcBMI() ? `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!` : `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`)
