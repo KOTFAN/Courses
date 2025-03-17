c6981227ca707e974d56978d91e82824f425195f
KOTFAN <kotfan.empire@gmail.com>
2025-03-17 21:01:35 +0200

    add all functionality for modal

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,30 @@
 'use strict';
+//page elements
+const bntsShowModal = document.querySelectorAll('.show-modal')
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
+const btnCloseModal = document.querySelector('.close-modal')
+
+
+const openModal = () => {
+   modal.classList.remove('hidden')
+   overlay.classList.remove('hidden')
+}
+
+const closeModal = () => {
+   modal.classList.add('hidden')
+   overlay.classList.add('hidden')
+}
+
+bntsShowModal.forEach((b) => b.addEventListener('click', openModal))
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', (e) => {
+
+   if (e.key === 'Escape' || !modal.classList.contains('hidden')) {
+      closeModal();
+   }
+})
+
