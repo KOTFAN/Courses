23eab4fed1ce52dd4c6a07a6b5f63d85fd81e9ed
KOTFAN <kotfan.empire@gmail.com>
2025-03-26 19:43:40 +0200

    add: some progres

--- Understanding_of_TS/base.js
+++ Understanding_of_TS/base.js
@@ -1,1 +1,43 @@
 console.log("Hello TypeScript's World");
+var userName;
+userName = 'KOTFAN';
+var direction;
+direction = 'down';
+// direction = 'j'
+console.log(direction);
+var user = {
+    name: 'Kotfan',
+    age: 20,
+};
+console.log(user);
+var v;
+v = 'not null or undefined';
+v = true;
+var DaysOfTheWeek;
+(function (DaysOfTheWeek) {
+    DaysOfTheWeek["Monday"] = "Monday";
+    DaysOfTheWeek["Tuesday"] = "Tuesday";
+    DaysOfTheWeek["Wednesday"] = "Wednesday";
+    DaysOfTheWeek["Thursday"] = "Thursday";
+    DaysOfTheWeek["Friday"] = "Friday1";
+    DaysOfTheWeek["Saturday"] = "Saturday";
+    DaysOfTheWeek["Sunday"] = "Sunday";
+})(DaysOfTheWeek || (DaysOfTheWeek = {}));
+var UserRole = 'blocked';
+console.log(UserRole);
+function showError(errorMesage) {
+    console.log(errorMesage);
+    throw new Error(errorMesage);
+}
+function log(message) {
+    console.log(message);
+}
+function add(a, b) {
+    return a + b;
+}
+// let a = showError('asdfghjkl')
+// console.log(a)
+function sendRequest(cb) {
+    cb('Request done');
+}
+sendRequest(log);
--- Understanding_of_TS/base.ts
+++ Understanding_of_TS/base.ts
@@ -1,1 +1,72 @@
 console.log("Hello TypeScript's World")
+
+
+let userName: string;
+userName = 'KOTFAN';
+let direction: 'left' | 'right' | 'up' | 'down';
+direction = 'down'
+// direction = 'j'
+
+console.log(direction)
+
+
+
+const user: {
+   name: string | undefined,
+   age: number | undefined,
+} = {
+   name: 'Kotfan',
+   age: 20,
+}
+
+console.log(user)
+
+
+let v: {};
+v = 'not null or undefined'
+v = true
+
+
+
+enum DaysOfTheWeek {
+   Monday = 'Monday',
+   Tuesday = 'Tuesday',
+   Wednesday = 'Wednesday',
+   Thursday = 'Thursday',
+   Friday = 'Friday1',
+   Saturday = 'Saturday',
+   Sunday = 'Sunday',
+}
+
+type Role = 'admin' | 'editor' | 'guest' | 'blocked';
+
+let UserRole: Role = 'blocked';
+
+console.log(UserRole)
+
+
+
+
+function showError(errorMesage: string): never {
+   console.log(errorMesage);
+   throw new Error(errorMesage);
+}
+
+function log(message: string) {
+   console.log(message)
+}
+
+function add(a: number, b: number): number {
+   return a + b;
+}
+
+// let a = showError('asdfghjkl')
+// console.log(a)
+
+
+function sendRequest(cb: (m: string) => void) {
+   cb('Request done')
+}
+
+
+sendRequest(log)
