f55790d4a53b790ff1b6b3abeab90423ea0c40cc
KOTFAN <kotfan.empire@gmail.com>
2025-04-15 14:43:48 +0300

    add Challenge #13

--- codingExercises/script.js
+++ codingExercises/script.js
@@ -405,7 +405,7 @@ The input will come from a textarea inserted into the DOM (see code below), and 
 THIS TEST DATA (pasted to textarea)
 underscore_case
  first_name
-Some_Variable 
+Some_Variable
   calculate_AGE
 delayed_departure
 
@@ -428,36 +428,125 @@ GOOD LUCK 😀
 
 
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
 
-const textarea = document.querySelector('textarea')
-
-
-
-document.querySelector('button').addEventListener('click', () => {
-   const inputData = textarea.value.split('\n')
-   const res = []
+// const textarea = document.querySelector('textarea')
 
 
-   for (const line of inputData) {
-      const lineData = line.toLowerCase().trim().split('_')
-      console.log(lineData)
-      const resLineArr = [];
-      for (let i = 0; i < lineData.length; i++) {
-         lineData[i] = lineData[i].trim()
-         if (lineData[i] !== '') {
-            if (i === 0) {
-               resLineArr.push(lineData[i])
-            } else {
-               resLineArr.push(lineData[i][0].toUpperCase() + lineData[i].slice(1))
-            }
 
-         }
+// document.querySelector('button').addEventListener('click', () => {
+//    const inputData = textarea.value.split('\n')
+//    const res = []
 
+
+//    for (const line of inputData) {
+//       const lineData = line.toLowerCase().trim().split('_')
+//       console.log(lineData)
+//       const resLineArr = [];
+//       for (let i = 0; i < lineData.length; i++) {
+//          lineData[i] = lineData[i].trim()
+//          if (lineData[i] !== '') {
+//             if (i === 0) {
+//                resLineArr.push(lineData[i])
+//             } else {
+//                resLineArr.push(lineData[i][0].toUpperCase() + lineData[i].slice(1))
+//             }
+
+//          }
+
+//       }
+//       res.push(resLineArr.join(''))
+//    }
+//    console.log(res.join('\n'))
+// })
+
+
+// Challenge #13
+
+
+/*
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+GOOD LUCK 😀
+*/
+
+const poll = {
+   question: 'What is your favourite programming language?',
+
+   options: [
+      '0: JavaScript',
+      '1: Python',
+      '2: Rust',
+      '3: C++'],
+   answers: new Array(4).fill(0),
+   registerNewAnswer() {
+      const favouriteProgramingLenguageNum = Number(window.prompt(this.question + '\n' + this.options.join('\n')))
+      if (this.options[favouriteProgramingLenguageNum]) {
+         this.answers[favouriteProgramingLenguageNum]++
+         this.displayResults('array')
       }
-      res.push(resLineArr.join(''))
-   }
-   console.log(res.join('\n'))
-})
+   },
 
+   displayResults(type = 'array') {
+      if (type === 'array') {
+         console.log(this.answers)
+      }
+      if (type === 'string') {
+         console.log(`Poll results are ${this.answers.join(' ')}`)
+      }
+   }
+}
+
+const answerThePool = document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
+
+let someData = [1, 2, 3, , 4, 5, 6, 6, , 7, 7,]
+
+poll.displayResults.call({ answers: someData }, 'string')
+
+
+
+
+
+
+
+
+
+
+// Challenge #14
+
+
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
