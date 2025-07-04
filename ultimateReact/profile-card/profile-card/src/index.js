131be74ad1dd6030feff21bd6cc0c3a00ba3f8b2
KOTFAN <kotfan.empire@gmail.com>
2025-07-04 17:21:04 +0300

    add SkillList, Skill components with styling

--- ultimateReact/profile-card/profile-card/src/index.js
+++ ultimateReact/profile-card/profile-card/src/index.js
@@ -18,19 +18,59 @@ function App() {
 function Avatar() {
   return (
     <div>
-      <img src="" alt="" />
+      <img className="avatar" src="./images/profile_image.jpg" alt="ME" />
     </div>
   );
 }
 
 function Intro() {
-  return <div></div>;
+  return (
+    <>
+      <h1>Vitaliy Petrovskiy</h1>
+      <p>
+        I'm a Front-End Developer passionate about building applications that
+        live, breathe, and inspire. Beyond technical skills, I bring energy,
+        leadership, and a relentless drive for improvement into every project
+        and every team I join.
+      </p>
+    </>
+  );
 }
 
 function SkillList() {
-  return <div></div>;
+  const skills = [
+    { name: "HTML", status: "Pro", id: 0, color: "#e34c26" },
+    { name: "CSS", status: "Pro", id: 1, color: "#264de4" },
+    { name: "SCSS", status: "Good", id: 2, color: "#cd6799" },
+    { name: "JavaScript", status: "Pro", id: 3, color: "#f0db4f" },
+    { name: "React", status: "Good", id: 4, color: "#61dafb" },
+    { name: "TypeScript", status: "Good", id: 5, color: "#3178c6" },
+    { name: "Git", status: "Pro", id: 6, color: "#f1502f" },
+    { name: "SQL", status: "Noob", id: 7, color: "#e38c00" },
+  ];
+  return (
+    <div className="skill-list">
+      {skills.map(({ name, status, id, color }) => (
+        <Skill name={name} status={status} key={id} color={color} />
+      ))}
+    </div>
+  );
 }
 
+function Skill({ name, status, color }) {
+  const statusEmoji = {
+    Pro: "💪",
+    Good: "👍",
+    Noob: "🙀",
+  };
+
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <h4>{name}</h4>
+      <p>{statusEmoji[status]}</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
