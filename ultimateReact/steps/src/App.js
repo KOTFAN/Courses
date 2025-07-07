d622238d215a5328e9983ea506b2ff69f7c2520f
KOTFAN <kotfan.empire@gmail.com>
2025-07-07 14:29:00 +0300

    add app markup

--- ultimateReact/steps/src/App.js
+++ ultimateReact/steps/src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 function App() {
-  return <div>hello react</div>;
+  const step = 3;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">
+        Step {step}:{messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950ff", color: "#fff" }}>
+          prev
+        </button>
+        <button style={{ backgroundColor: "#7950ff", color: "#fff" }}>
+          next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
