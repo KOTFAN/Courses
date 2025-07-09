e1d626fba52713df5ff51dad446668485f4adf27
KOTFAN <kotfan.empire@gmail.com>
2025-07-09 10:26:58 +0300

    add starter

---
+++ ultimateReact/far-away/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌅Far Away🌄</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your treap👀?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have N items on your list, and you already packed X(X%)</em>
+    </footer>
+  );
+}
+
+export default App;
