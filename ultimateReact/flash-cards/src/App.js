c0d61e2d5e63c9774fbb5a42a0c4c1e1785250ea
KOTFAN <kotfan.empire@gmail.com>
2025-07-09 13:36:57 +0300

    finsih flash-cards progect

---
+++ ultimateReact/flash-cards/src/App.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FlashCards />
+    </div>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+function FlashCards() {
+  const [activeElement, setActiveElement] = useState(null);
+
+  return (
+    <div
+      className="flashcards"
+      onClick={(e) => {
+        console.log(e.target.id);
+        if (activeElement === Number(e.target.id)) {
+          console.log("again");
+          setActiveElement(null);
+        } else {
+          setActiveElement(Number(e.target.id));
+        }
+      }}
+    >
+      {questions.map(({ id, question, answer }) => {
+        return (
+          <FlashCard
+            key={id}
+            question={question}
+            answer={answer}
+            index={id}
+            activeElement={activeElement}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function FlashCard({ question, answer, activeElement, index }) {
+  const isActive = index === activeElement;
+
+  return (
+    <div className={isActive ? "selected" : ""} id={index}>
+      {isActive ? answer : question}
+    </div>
+  );
+}
