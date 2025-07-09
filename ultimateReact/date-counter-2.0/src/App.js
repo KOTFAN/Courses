f592ee598e2541a05bf7333af5d62add34aac46e
KOTFAN <kotfan.empire@gmail.com>
2025-07-09 15:30:10 +0300

    add starter

---
+++ ultimateReact/date-counter-2.0/src/App.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [stepSize, setStepSize] = useState(1);
+  const [stepsCount, setStepsCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="counter">
+        <button
+          onClick={() => {
+            if (stepSize > 1) {
+              setStepSize((currentSize) => currentSize - 1);
+            }
+          }}
+        >
+          -
+        </button>
+        <p>Step: {stepSize}</p>
+        <button onClick={() => setStepSize((currentSize) => currentSize + 1)}>
+          +
+        </button>
+      </div>
+      <div className="counter">
+        <button
+          onClick={() => setStepsCount((currenCount) => currenCount - stepSize)}
+        >
+          -
+        </button>
+        <p>Count: {stepsCount}</p>
+        <button
+          onClick={() => setStepsCount((currenCount) => currenCount + stepSize)}
+        >
+          +
+        </button>
+      </div>
+      <ShowDate stepsCount={stepsCount} />
+    </div>
+  );
+}
+
+function ShowDate({ stepsCount }) {
+  const date = new Date();
+  if (stepsCount === 0) return <p>Today is: {date.toDateString()}</p>;
+
+  //can be refactored to one conditional
+  if (stepsCount < 0)
+    return (
+      <p>
+        {Math.abs(stepsCount)} days ago was:{" "}
+        {new Date(
+          date.getTime() + stepsCount * 1000 * 60 * 60 * 24
+        ).toDateString()}
+      </p>
+    );
+  //stepsCount > 0
+  else {
+    return (
+      <p>
+        In {stepsCount} days will be:
+        {new Date(
+          date.getTime() + stepsCount * 1000 * 60 * 60 * 24
+        ).toDateString()}
+      </p>
+    );
+  }
+}
+
+export default App;
