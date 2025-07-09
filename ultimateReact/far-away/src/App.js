f24295ce4b7384f3832e6c815ecc47965d73c481
KOTFAN <kotfan.empire@gmail.com>
2025-07-09 12:47:28 +0300

    add state for Form, made input and select - controled elements

--- ultimateReact/far-away/src/App.js
+++ ultimateReact/far-away/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 const initialItems = [
@@ -23,19 +24,40 @@ function Logo() {
   return <h1>🌅Far Away🌄</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
   }
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip👀?</h3>
 
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          //  console.log(e.target.value);
+          setQuantity(Number(e.target.value));
+        }}
+      >
         {Array.from({ length: 20 }, (el, i) => {
-          return <option value={i + 1}>{i + 1}</option>;
+          return (
+            <option key={i + 1} value={i + 1}>
+              {i + 1}
+            </option>
+          );
         })}
       </select>
-      <input placeholder="your item..." />
+      <input
+        placeholder="your item..."
+        value={description}
+        onChange={(e) => {
+          //  console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      />
       <button>ADD</button>
     </form>
   );
