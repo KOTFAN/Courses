3e3eb00e05105003b74e66a3849299fd3e50df4a
KOTFAN <kotfan.empire@gmail.com>
2025-07-10 11:12:03 +0300

    add Accordion, AccordionItem components

---
+++ ultimateReact/accordion/src/components/Accordion.js
@@ -0,0 +1,11 @@
+import AccordionItem from "./AccordionItem";
+
+function Accordion() {
+  return (
+    <div className="accordion">
+      <AccordionItem />
+    </div>
+  );
+}
+
+export default Accordion;
---
+++ ultimateReact/accordion/src/components/AccordionItem.js
@@ -0,0 +1,9 @@
+export default function AccordionItem() {
+  return (
+    <div className="item">
+      <span className="number"> 01</span>
+      <h2 className="title">Where are these chairs assembled?</h2>
+      <span className="icon">+</span>
+    </div>
+  );
+}
