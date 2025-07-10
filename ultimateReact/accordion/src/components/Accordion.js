2a93cef45b9ea69db92c98bdbd4227d9edfc0900
KOTFAN <kotfan.empire@gmail.com>
2025-07-10 11:22:28 +0300

    Add props for Accordion

--- ultimateReact/accordion/src/components/Accordion.js
+++ ultimateReact/accordion/src/components/Accordion.js
@@ -1,9 +1,30 @@
 import AccordionItem from "./AccordionItem";
 
 function Accordion() {
+  const faqs = [
+    {
+      title: "Where are these chairs assembled?",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+    },
+    {
+      title: "How long do I have to return my chair?",
+      text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+    },
+    {
+      title: "Do you ship to countries outside the EU?",
+      text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+    },
+  ];
   return (
     <div className="accordion">
-      <AccordionItem />
+      {faqs.map(({ title, text }, index) => (
+        <AccordionItem
+          title={title}
+          text={text}
+          number={index + 1}
+          key={index}
+        />
+      ))}
     </div>
   );
 }
--- ultimateReact/accordion/src/components/AccordionItem.js
+++ ultimateReact/accordion/src/components/AccordionItem.js
@@ -1,9 +1,10 @@
-export default function AccordionItem() {
+export default function AccordionItem({ title, text, number }) {
   return (
     <div className="item">
-      <span className="number"> 01</span>
-      <h2 className="title">Where are these chairs assembled?</h2>
+      <span className="number"> {number > 9 ? number : "0" + number}</span>
+      <h2 className="title">{title}</h2>
       <span className="icon">+</span>
+      <div className="content-box">{text}</div>
     </div>
   );
 }
