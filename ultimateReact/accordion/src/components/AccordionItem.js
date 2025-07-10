34c5d13eb22874310429a8b756fdcfcda597e297
KOTFAN <kotfan.empire@gmail.com>
2025-07-10 11:43:52 +0300

    add state for AccordionItem

--- ultimateReact/accordion/src/components/AccordionItem.js
+++ ultimateReact/accordion/src/components/AccordionItem.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 export default function AccordionItem({ title, text, number }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleOpenClose() {
+    setIsOpen((c) => !c);
+  }
   return (
-    <div className="item">
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleOpenClose}>
       <span className="number"> {number > 9 ? number : "0" + number}</span>
       <h2 className="title">{title}</h2>
-      <span className="icon">+</span>
-      <div className="content-box">{text}</div>
+      <span className="icon">{isOpen ? "-" : "+"}</span>
+      {isOpen ? <div className="content-box">{text}</div> : null}
     </div>
   );
 }
