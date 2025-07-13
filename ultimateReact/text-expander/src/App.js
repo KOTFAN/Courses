dbb3b0351b4ab5db4f8341772462a1d4d972747b
KOTFAN <kotfan.empire@gmail.com>
2025-07-13 18:12:13 +0300

    add TextExpander minimal logic

--- ultimateReact/text-expander/src/App.js
+++ ultimateReact/text-expander/src/App.js
@@ -1,9 +1,10 @@
-import "./styles.css";
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div>
-      <TextExpander>
+      <TextExpander shownWordsNumber={1}>
         Space travel is the ultimate adventure! Imagine soaring past the stars
         and exploring new worlds. It's the stuff of dreams and science fiction,
         but believe it or not, space travel is a real thing. Humans and robots
@@ -12,7 +13,7 @@ export default function App() {
       </TextExpander>
 
       <TextExpander
-        collapsedNumWords={20}
+        shownWordsNumber={20}
         expandButtonText="Show text"
         collapseButtonText="Collapse text"
         buttonColor="#ff6622"
@@ -24,7 +25,7 @@ export default function App() {
         foot on the moon or when rovers were sent to roam around on Mars.
       </TextExpander>
 
-      <TextExpander expanded={true} className="box">
+      <TextExpander open={true} className="box">
         Space missions have given us incredible insights into our universe and
         have inspired future generations to keep reaching for the stars. Space
         travel is a pretty cool thing to think about. Who knows what we'll
@@ -34,6 +35,25 @@ export default function App() {
   );
 }
 
-function TextExpander() {
-  return <div>TODO</div>;
+function TextExpander({
+  children,
+  open = false,
+  shownWordsNumber = 20,
+  className,
+  expandButtonText,
+  collapseButtonText,
+  buttonColor,
+}) {
+  const [isOpen, setIsOpen] = useState(open);
+
+  const text = children.split(" ");
+  console.log(text);
+
+  return (
+    <div className={className}>
+      {shownWordsNumber < text.length
+        ? text.filter((w, i) => i < shownWordsNumber).join(" ") + "..."
+        : children}
+    </div>
+  );
 }
