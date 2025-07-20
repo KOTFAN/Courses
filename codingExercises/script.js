a47fbd84c281391f17789e29bfb1996dbff0b1cb
KOTFAN <kotfan.empire@gmail.com>
2025-07-20 17:49:39 +0300

    add Challenge #26

--- codingExercises/script.js
+++ codingExercises/script.js
@@ -1068,31 +1068,101 @@ TEST COORDINATES 2: -33.933, 18.474
 GOOD LUCK 😀
 */
 
-const bnt = document.getElementById("send-request");
-const latInput = document.getElementById("lat");
-const lngInput = document.getElementById("lng");
+// const bnt = document.getElementById("send-request");
+// const latInput = document.getElementById("lat");
+// const lngInput = document.getElementById("lng");
 
-function whereAmI(lat = 0, lng = 0) {
-  fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
-  )
-    .then((data) => {
-      if (!data.ok) {
-        throw new Error("failed to fetch data");
-      }
-      return data.json();
-    })
-    .then((location) => {
-      if (location.countryName) {
-        console.log(`You are in ${location.countryName}`);
-      } else {
-        console.log("invalid input");
-      }
-    })
-    .catch((err) => console.log(err));
+// function whereAmI(lat = 0, lng = 0) {
+//   fetch(
+//     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+//   )
+//     .then((data) => {
+//       if (!data.ok) {
+//         throw new Error("failed to fetch data");
+//       }
+//       return data.json();
+//     })
+//     .then((location) => {
+//       if (location.countryName) {
+//         console.log(`You are in ${location.countryName}`);
+//       } else {
+//         console.log("invalid input");
+//       }
+//     })
+//     .catch((err) => console.log(err));
+// }
+// // whereAmI(47.508, 30.381);
+
+// bnt.onclick = () => {
+//   whereAmI(Number(latInput.value), Number(lngInput.value));
+// };
+
+///////////////////////////////////////
+// Coding Challenge #26
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new 
+image (use document.createElement('img')) and sets the .src attribute to the provided image path. 
+When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. 
+The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+
+function wait(s) {
+  //promise will resolve(call cb in then) in s seconds
+  return new Promise(function (resolve) {
+    setTimeout(resolve, 1000 * s);
+  });
 }
-// whereAmI(47.508, 30.381);
 
-bnt.onclick = () => {
-  whereAmI(Number(latInput.value), Number(lngInput.value));
-};
+function createImage(imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", () => {
+      document.body.append(img);
+      resolve(img); //img here is argument for cb in then
+    });
+
+    img.addEventListener("error", () => {
+      reject(new Error(`Failed to load image: ${imgPath}`));
+    });
+  });
+}
+let currentImage;
+createImage("./img/img-1.jpg")
+  .then((imgEl) => {
+    currentImage = imgEl;
+    return wait(2); //is new promise
+  })
+  .then(() => {
+    currentImage.style.display = "none";
+    return createImage("./img/img-2.jpg"); //is new promise
+  })
+  .then((imgEl) => {
+    currentImage = imgEl;
+    return wait(2); //is new promise
+  })
+  .then(() => {
+    currentImage.style.display = "none";
+    return createImage("./img/img-3.jpg"); //is new promise
+  })
+  .catch((err) => console.log(err));
