5f99fd0bd3301c36eb678ab1d4c5e41e17777ccf
KOTFAN <kotfan.empire@gmail.com>
2025-08-14 15:35:51 +0300

    add showSwappedValue

--- ultimateReact/currency-converter/src/App.js
+++ ultimateReact/currency-converter/src/App.js
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 export default function App() {
   const [amount, setAmount] = useState(0);
   const [firstCurrency, setFirstCurrency] = useState("USD");
-  const [secondCurrency, setSecondCurrency] = useState("CAD");
+  const [secondCurrency, setSecondCurrency] = useState("USD");
+  const [convertedValue, setConvertedValue] = useState(0);
+
+  useEffect(() => {
+    async function showSwappedValue() {
+      try {
+        if (firstCurrency === secondCurrency) {
+          setConvertedValue(amount);
+          return;
+        } else {
+          const data = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${firstCurrency}&to=${secondCurrency}`
+          );
+          const result = await data.json();
+          setConvertedValue(result.rates[secondCurrency]);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    showSwappedValue();
+  }, [amount, firstCurrency, secondCurrency]);
   return (
     <div>
       <input
@@ -30,7 +51,9 @@ export default function App() {
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>
+        {convertedValue} to {secondCurrency}
+      </p>
     </div>
   );
 }
