37e9107fe6823610989c6b7b6677f3537f806efe
KOTFAN <kotfan.empire@gmail.com>
2025-08-14 12:25:00 +0300

    make input and selects controled elements

--- ultimateReact/currency-converter/src/App.js
+++ ultimateReact/currency-converter/src/App.js
@@ -1,16 +1,30 @@
+import { useState } from "react";
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 export default function App() {
+  const [amount, setAmount] = useState(0);
+  const [firstCurrency, setFirstCurrency] = useState("USD");
+  const [secondCurrency, setSecondCurrency] = useState("CAD");
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select
+        value={firstCurrency}
+        onChange={(e) => setFirstCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={secondCurrency}
+        onChange={(e) => setSecondCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
