2eb2e375c8d38ef684915c43207aeb8650f38da9
KOTFAN <kotfan.empire@gmail.com>
2025-08-16 16:57:03 +0300

    add Header and Main components

--- ultimateReact/react-quiz/src/App.js
+++ ultimateReact/react-quiz/src/App.js
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/20</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
