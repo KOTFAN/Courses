18d3861624d7deb17dbcb21854db69d8dbac4920
KOTFAN <kotfan.empire@gmail.com>
2025-08-16 11:49:21 +0300

    add useGeolocation

---
+++ ultimateReact/geolocation/src/hooks/useGeolocation.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export function useGeolocation(cb) {
+  const [position, setPosition] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    cb((count) => count + 1);
+
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return { position, isLoading, error, getPosition };
+}
